5530f5e358f29c52e2bc92951754cd48fa996891
Jana Veliskova <veliskova@gmail.com>
2024-08-23 23:14:44 -0400

    finish the elements and do simple styling

--- zooproblem/main.js
+++ zooproblem/main.js
@@ -31,8 +31,6 @@
  
 
         animals.forEach(animal => {
- 
-
             // clone the section and insert it into main
             const section = animalArea.content.cloneNode(true);
 
@@ -40,27 +38,30 @@
             let h2 = section.querySelectorAll("h2");
             h2[0].textContent = animal.animal;
 
+            // create the image
+            let img = section.querySelectorAll("img");
+            img[0].setAttribute("src", animal.imgurl);
+            img[0].setAttribute("alt", animal.imgalt);
+
+
             // grab the li, clone it, and loop through names to append list items
             let ul = section.querySelectorAll("ul");
 
             // create the list items and pop the names of the animals in
             for (let i = 0; i < animal.names.length; i++) {
-                ul[0].insertAdjacentHTML("beforeend", '<li>' + animal.names[i] + '</li>');
+                ul[0].insertAdjacentHTML("beforeend", '<li class="animal-card__name">' + animal.names[i] + '</li>');
             }
 
+            // add description
+            let paragraph = section.querySelectorAll("p");
+            paragraph[0].textContent = animal.desc;
+
+            // fill in the link for more info
+            let url = section.querySelectorAll("a");
+            url[0].setAttribute("href", animal.linkurl);
+            url[0].textContent = animal.linktext;
+
             elementToAttachTo.appendChild(section);
-
-
-            // elementToAttachTo = appendElement("section", animal.animal, elementToAttachTo);
-            
-            // elementToAttachTo.appendChild(newElement);
-            // elementToAttachTo = newElement;
-            
-            
-
-
-
-
         });
 
 
