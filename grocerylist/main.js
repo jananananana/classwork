41cc6abab38efed95b582c206b1f2cb2d14def0f
Jana Veliskova <veliskova@gmail.com>
2024-08-28 22:14:35 -0400

    adding in some data

--- grocerylist/main.js
+++ grocerylist/main.js
@@ -1,14 +1,19 @@
 (function (){
     const appFunctions = (function() {
         
-        const _getGroceryData = () => {
-            return console.log("hi jana");
+        const _categorizeGroceries = (data) => {
+            return console.log(data);
+        }
+        
+        const _getGroceryData  = async () => {
+            const data = await fetch ('./Groceries.json').then((data) => data.json());
+            _categorizeGroceries(data);
         }
 
         const _init = () => {
             _getGroceryData();
-        }
-
+        }    
+        
         return {
             init: function() {_init()}
         };
