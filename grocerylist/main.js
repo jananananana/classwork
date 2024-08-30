f868aa24b488e2358f0a0f6a377a30d614264a13
Jana Veliskova <veliskova@gmail.com>
2024-08-29 21:56:04 -0400

    ugh whatever

--- grocerylist/main.js
+++ grocerylist/main.js
@@ -4,12 +4,27 @@
         const _categorizeGroceries = (data) => {
             let sectionArr = [];
 
+
             for (let i = 0; i < data.length; i++) {
                 const element = data[i];
+
+                if (!sectionArr.includes(element.area)) {
+                    sectionArr.push(element.area);
+                }
+
+                console.log(sectionArr);
+
+
+                console.log(element);
+                console.log(_checkForSection(data[i]), sectionArr);
                 
             }
 
             return console.log(data);
+        }
+
+        const _checkForSection = (groceryitem, sectionArr) => {
+
         }
         
         const _getGroceryData  = async () => {
